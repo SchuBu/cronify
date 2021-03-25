0ab1baf030dac79a07918c4d1457209ee9ce88d7
Peter Schulze-Buxloh <peter@schu-bu.de>
2021-03-25 18:31:55 +0100

    added HasFactory trait

--- src/Models/Cron.php
+++ src/Models/Cron.php
@@ -2,10 +2,13 @@
 
 namespace Schubu\Cronify\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cron extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
     public $timestamps = false;
 
